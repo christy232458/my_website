4b639e09e71fd1c20e4d06b861439dadc0db3d9b
christy232458 <194615352+christy232458@users.noreply.github.com>
2025-02-25 21:04:27 +0800

    added email functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+// Variables
+    // Contact page
+        // --Email--
+        let emailForm = document.getElementById('email-form');
+        let emailIcon = document.querySelector('#email a');
+        let formClose = document.getElementById('email-close')
+        let submitEmail = document.getElementById('email-submit')
+        let emailMe = document.querySelector('#email-me h2')
+        // ----------------
+
+// Functions
+    // Contact page
+        // --Email--
+        let showEmailForm = function() {
+            emailIcon.style.visibility = 'hidden';
+            emailMe.style.visibility = 'hidden';
+            emailForm.style.visibility = 'visible';
+        }
+
+        let hideEmailForm = function() {
+            emailIcon.style.visibility = 'visible';
+            emailMe.style.visibility = 'visible';
+            emailForm.style.visibility = 'hidden';
+        }
+
+        let emailSubmission = function(event) {
+            event.preventDefault();
+
+            let senderName = document.getElementById("email-name").value;
+            let senderSubject = document.getElementById("email-subject").value;
+            let senderMessage = document.getElementById("email-message").value;
+
+            if (!senderName || !senderSubject || !senderMessage) {
+                alert("Please fill in all fields.");
+                return;
+            }
+            
+            let emailSubject = `${senderSubject} [${senderName}]`;
+
+            let mailtoLink = `mailto:padma@padmachristie.com?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(senderMessage)}`;
+
+            window.location.href = mailtoLink;
+        }
+        // ----------------
+
+// Events
+    // Contact page
+        // --Email--
+        emailIcon.onclick = showEmailForm;
+        formClose.onclick = hideEmailForm;
+        submitEmail.onclick = emailSubmission;
+        // ----------------
